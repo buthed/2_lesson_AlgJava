5a30354ffc20ff3865ef4b5739901888891c6b74
buthed <temaTihonov@yandex.ru>
2021-03-17 22:55:11 +0300

    All tasks

--- src/com/tematihonov/Main.java
+++ src/com/tematihonov/Main.java
@@ -1,6 +1,5 @@
 package com.tematihonov;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -58,16 +57,53 @@ public class Main {
             mArrayTask3[i] = random.nextInt(30);
         }
         mArrayTask3Copy = Arrays.copyOf(mArrayTask3, mArrayTask3.length);
+        System.out.println("Вывод заданного массива: " + Arrays.toString(mArrayTask3Copy));
         startTime = System.nanoTime();
         Arrays.sort(mArrayTask3Copy);
-        endTime = System.nanoTime() - startTime;
-        System.out.println("Время выполнения метода(Arrays.sort())= " + endTime);
+        long arraysSortEndTime = System.nanoTime() - startTime;
+        System.out.println("Вывод отсортированного массива: " + Arrays.toString(mArrayTask3Copy));
+        System.out.println("Время выполнения метода(Arrays.sort())= " + arraysSortEndTime);
         System.out.println();
 
 
         System.out.println("Task 2.4");
+        mArrayTask3Copy = Arrays.copyOf(mArrayTask3, mArrayTask3.length);   //убираем сортировку
+        System.out.println("Вывод заданного массива: " + Arrays.toString(mArrayTask3Copy));
+        startTime = System.nanoTime();
+        bubbleSort(mArrayTask3Copy);
+        long bubbleSortEndTime = System.nanoTime() - startTime;
+        System.out.println("Вывод отсортированного массива: " + Arrays.toString(mArrayTask3Copy));
+        System.out.println("Время выполнения пузырьковой сортировки = " + bubbleSortEndTime);
+        System.out.println();
 
 
+
+        System.out.println("Task 2.5");
+        mArrayTask3Copy = Arrays.copyOf(mArrayTask3, mArrayTask3.length);   //убираем сортировку
+        System.out.println("Вывод заданного массива: " + Arrays.toString(mArrayTask3Copy));
+        startTime = System.nanoTime();
+        selectionSort(mArrayTask3Copy);
+        long selectionSortEndTime = System.nanoTime() - startTime;
+        System.out.println("Вывод отсортированного массива: " + Arrays.toString(mArrayTask3Copy));
+        System.out.println("Время выполнения сортировки методом выбора = " + selectionSortEndTime);
+        System.out.println();
+
+        System.out.println("Task 2.6");
+        mArrayTask3Copy = Arrays.copyOf(mArrayTask3, mArrayTask3.length);   //убираем сортировку
+        System.out.println("Вывод заданного массива: " + Arrays.toString(mArrayTask3Copy));
+        startTime = System.nanoTime();
+        insertionSort(mArrayTask3Copy);
+        long insertionSortEndTime = System.nanoTime() - startTime;
+        System.out.println("Вывод отсортированного массива: " + Arrays.toString(mArrayTask3Copy));
+        System.out.println("Время выполнения сортировки методом вставки = " + insertionSortEndTime);
+        System.out.println();
+
+        System.out.println("Вывод.");
+        System.out.println("Сравнение время выполнения сортировки:");
+        System.out.println("Время выполнения метода(Arrays.sort())= " + arraysSortEndTime);
+        System.out.println("Время выполнения пузырьковой сортировки = " + bubbleSortEndTime);
+        System.out.println("Время выполнения сортировки методом выбора = " + selectionSortEndTime);
+        System.out.println("Время выполнения сортировки методом вставки = " + insertionSortEndTime);
     }
 
     public static int linearSearch(int arr[], int elementToSearch) {
@@ -92,5 +128,44 @@ public class Main {
                 lastIndex = middleIndex - 1;
         }
         return -1;
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int pos = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    pos = j;
+                    min = arr[j];
+                }
+            }
+            arr[pos] = arr[i];
+            arr[i] = min;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int buff = arr[i];
+            int in = i;
+            while (in > 0 && arr[in-1] >= buff) {
+                arr[in] = arr[in-1];
+                in--;
+            }
+            arr[in + 1] = buff;
+        }
     }
 }
